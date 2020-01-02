59769ff0fd4383da3a7ee742f5941773f943d5e4
barasztamas <barasztamas@users.noreply.github.com>
2020-01-02 11:15:21 +0100

    kötelező elemek footer-be

--- index.php
+++ index.php
@@ -35,15 +35,5 @@ require_once("_init.php");
         <table id="board"></table>
     </div>
 
-    <br/>Bárász Tamás
-    <br/>S8JPUQ
-    <br/>Webprogramozás esti, Js beadandó, Küldöncök
-    <br/>2019-10-10
-    <br/>Ezt a megoldást Bárász Tamás - S8JPUQ küldte be és készítette a Webprogramozás esti kurzus Js beadandó - Küldöncök feladatához.
-    <br/>Kijelentem, hogy ez a megoldás a saját munkám.
-    <br/>Nem másoltam vagy használtam harmadik féltől származó megoldásokat.
-    <br/>Nem továbbítottam megoldást hallgatótársaimnak, és nem is tettem közzé.
-    <br/>Az Eötvös Loránd Tudományegyetem Hallgatói Követelményrendszere (ELTE szervezeti és működési szabályzata, II. Kötet, 74/C. §) kimondja, hogy mindaddig, amíg egy hallgató egy másik hallgató munkáját - vagy legalábbis annak jelentős részét - saját munkájaként mutatja be, az fegyelmi vétségnek számít. A fegyelmi vétség legsúlyosabb következménye a hallgató elbocsátása az egyetemről.
-
     <script src="index.js" type="module"></script>
 <?php require("partials/footer.php"); ?>
--- partials/footer.php
+++ partials/footer.php
@@ -1,1 +1,11 @@
+    <br/>Bárász Tamás
+    <br/>S8JPUQ
+    <br/>Webprogramozás esti, Js beadandó, Küldöncök
+    <br/>2019-10-10
+    <br/>Ezt a megoldást Bárász Tamás - S8JPUQ küldte be és készítette a Webprogramozás esti kurzus Js beadandó - Küldöncök feladatához.
+    <br/>Kijelentem, hogy ez a megoldás a saját munkám.
+    <br/>Nem másoltam vagy használtam harmadik féltől származó megoldásokat.
+    <br/>Nem továbbítottam megoldást hallgatótársaimnak, és nem is tettem közzé.
+    <br/>Az Eötvös Loránd Tudományegyetem Hallgatói Követelményrendszere (ELTE szervezeti és működési szabályzata, II. Kötet, 74/C. §) kimondja, hogy mindaddig, amíg egy hallgató egy másik hallgató munkáját - vagy legalábbis annak jelentős részét - saját munkájaként mutatja be, az fegyelmi vétségnek számít. A fegyelmi vétség legsúlyosabb következménye a hallgató elbocsátása az egyetemről.
+
 </body>
